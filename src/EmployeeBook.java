4b6f43c508117eadfb1745a6ecf4e4f1afc24eff
Your Name <you@example.com>
2024-10-15 14:01:43 +0300

    completed base complexity

---
+++ src/EmployeeBook.java
@@ -0,0 +1,17 @@
+public class EmployeeBook {
+    private String[] employeeArray = new String[2] ;
+
+    public EmployeeBook(String[] employeeArray) {
+        this.employeeArray = employeeArray;
+    }
+
+    public String[] getEmployeeArray() {
+        return employeeArray;
+    }
+
+    public void setEmployeeArray(String[] employeeArray) {
+        this.employeeArray = employeeArray;
+    }
+
+
+}
